155cb22358958ca94703ffacd074d241faa3a976
macsoftechnologies <macsoftechnologies@gmail.com>
2022-04-02 14:10:36 +0530

    code commiited

--- src/app/user/confirm-testride/confirm-testride.component.ts
+++ src/app/user/confirm-testride/confirm-testride.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-testride',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmTestrideComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  backtohome() {
+    this.router.navigateByUrl('/user/Home')
   }
 
 }

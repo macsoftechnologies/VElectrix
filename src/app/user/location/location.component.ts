3624af46eb9cb2ff0c51fdbc8bec94b1ca09eb1f
macsoftechnologies <macsoftechnologies@gmail.com>
2022-05-04 10:38:05 +0530

    code commiited and changess made in location component

--- src/app/user/location/location.component.ts
+++ src/app/user/location/location.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VElectrixService } from 'src/app/Services/velectrix.service';
 
 @Component({
   selector: 'app-location',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.scss']
 })
 export class LocationComponent implements OnInit {
+  chargerList: any;
 
-  constructor() { }
+  constructor(private vElectrixSerivces: VElectrixService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getChargersList();
+  }
+
+  getChargersList() {
+    this.vElectrixSerivces.getChargerList().subscribe((chargerResp) => {
+      if(chargerResp.StatusCode == 200) {
+        this.chargerList = chargerResp.Data.UserDetails
+        console.log("Charger List", this.chargerList)
+      }
+    })
   }
 
 }

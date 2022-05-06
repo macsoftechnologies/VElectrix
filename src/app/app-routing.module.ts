02f82fbc7c3a72a2acdc23c4f3d4427916e7c708
macsoftechnologies <macsoftechnologies@gmail.com>
2022-05-06 17:45:41 +0530

    success image upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,10 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),RouterModule.forRoot(routes, {
+    scrollPositionRestoration: "top",
+    useHash: true,
+  }),],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}

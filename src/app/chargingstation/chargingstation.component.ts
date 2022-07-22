5169298da5652e6d3cd498c4b9ac1a4a08af4e8a
macsoftechnologies <macsoftechnologies@gmail.com>
2022-07-22 15:11:08 +0530

    chnages made most of the components

--- src/app/chargingstation/chargingstation.component.ts
+++ src/app/chargingstation/chargingstation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { VElectrixService } from '../Services/velectrix.service';
+declare const $: any
 
 @Component({
   selector: 'app-chargingstation',
@@ -14,7 +15,7 @@ export class ChargingstationComponent implements OnInit {
   ChargerList: any;
   getPoints = [];
   markerslengths: any;
-  locationPoints: any =[];
+  locationPoints: any = [];
   getLatitude: any;
   getlongitude: any;
   constructor(private vElectrixServices: VElectrixService, private router: Router) { }
@@ -23,10 +24,10 @@ export class ChargingstationComponent implements OnInit {
 
     this.getChargersList();
 
-    
+
   }
 
-  getLocation(){
+  getLocation() {
     const mapElement = document.getElementById('map')
     if (mapElement) {
       const map = new google.maps.Map(mapElement, {
@@ -34,14 +35,18 @@ export class ChargingstationComponent implements OnInit {
         zoom: 10
       });
 
-      const markers = this.locationPoints
+      const markers = this.ChargerList
       console.log("markers", markers)
       markers.map((marker: { latitude: number; longitude: number }) => {
-        new google.maps.Marker({
+        const mapMarker = new google.maps.Marker({
           position: new google.maps.LatLng(marker.latitude, marker.longitude),
           map,
           icon: "../assets/images/marker.png"
         })
+        mapMarker.addListener('click', function () {
+          $("#exampleModal").modal('show')
+        })
+
       })
       // this.marker = new google.maps.Marker({
       //   position: this.myLatlng,
@@ -79,22 +84,22 @@ export class ChargingstationComponent implements OnInit {
 
   getChargersList() {
     this.vElectrixServices.getChargerList().subscribe((listResp) => {
-      if(listResp.StatusCode == 200) {
-        
+      if (listResp.StatusCode == 200) {
+
         this.ChargerList = listResp?.Data?.UserDetails
 
         this.getLatitude = listResp.Data.UserDetails.latitude
         this.getlongitude = listResp.Data.UserDetails.longitude
 
 
-        console.log("list",this.ChargerList)
-         this.ChargerList.forEach((item:any) => {
+        console.log("list", this.ChargerList)
+        this.ChargerList.forEach((item: any) => {
           this.locationPoints.push({ longitude: item.longitude, latitude: item.latitude })
         });
         this.getLocation()
-        console.log("arr",this.locationPoints)
+        console.log("arr", this.locationPoints)
         // this.getPoints = Object.assign({}, listResp.Data.UserDetails.location)
-        
+
         // console.log("List", this.ChargerList)
         // console.log("points", this.getPoints)
       }
--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -11,12 +11,15 @@ import { environment } from 'src/environments/environment';
 export class HomeComponent implements OnInit {
   getHomeImgList: any;
   baseUrl: string = environment.baseUrl
+  url: any;
 
 
   constructor(private VElectrixServices: VElectrixService, private router: Router) { }
 
   ngOnInit(): void {
     this.getBannersList();
+
+
   }
 
   getBannersList(){
@@ -24,5 +27,13 @@ export class HomeComponent implements OnInit {
       this.getHomeImgList = bannerResp.imageRes;
       console.log('image', this.getHomeImgList)
     })
+  }
+
+  Gotocomps(data: any) {
+    console.log(data.url)
+    // localStorage.setItem('URL', data.url)
+    this.url = data.url
+  
+    this.router.navigate(['user/' + this.url])
   }
 }
